82238a5f5b40792dc8114a05cf8aaffe5f9b3410
TANISHKA GUPTA <84371065+thetanishkagupta@users.noreply.github.com>
2022-08-02 20:58:43 +0530

    Update Linked List Insertion.py

--- Linked List Insertion.py
+++ Linked List Insertion.py
@@ -11,14 +11,28 @@ class Solution:
         temp.next = head
         head = temp
         return head
-           
+        
+    def insertAfter(self,nodeafter,x):
+        p = head
+        if p is None:
+            return 0
+        else:
+            while p.data != nodeafter:
+                p = p.next
+            if p is None:
+                return 0
+            temp = Node(x)
+            temp.next = p.next
+            p.next = temp
+        return head 
+        
     #Function to insert a node at the end of the linked list.
     def insertAtEnd(self,head,x):
         p = head
-        if p is None:           # if no node exist in the linked list
+        if p is None:        # if no node exist in the linked list
             temp = Node(x)
             head = temp
-        else:                   # if node is present in the linked list to insert at the end                      
+        else:                 # if node is present in the linked list to insert at the end
             while p.next != None:
                 p = p.next
             temp = Node(x)
