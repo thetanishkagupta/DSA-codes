ece5873b257a6553e218823ebc1427fc6828c848
TANISHKA GUPTA <84371065+thetanishkagupta@users.noreply.github.com>
2022-08-09 21:15:55 +0530

    Create Reverse Linked List.py

---
+++ Reverse Linked List.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def reverseList(self, head):
+        prev = None
+        next = None
+        curr = head
+        while curr!= None:
+            next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
+        head = prev
+        return head
+        
