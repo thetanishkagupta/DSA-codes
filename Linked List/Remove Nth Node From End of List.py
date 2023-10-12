9decaf5f34fd6cca4bf95f51a98685b5af64366c
TANISHKA GUPTA <84371065+thetanishkagupta@users.noreply.github.com>
2023-10-12 15:10:04 +0530

    Update Remove Nth Node From End of List.py

--- Linked List/Remove Nth Node From End of List.py
+++ Linked List/Remove Nth Node From End of List.py
@@ -14,3 +14,22 @@ class Solution(object):
             prev = prev.next
         prev.next = prev.next.next
         return head
+
+
+# By using two pinter approach
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        slow = head
+        fast = head
+        for i in range(n):  # move fast pointer till Nth term (fast pointer is N nodes ahead)
+            fast = fast.next
+        if fast == None:     # means node to be removed is head of list
+            return slow.next
+        while(fast.next != None):  
+            slow = slow.next
+            fast= fast.next
+        slow.next = slow.next.next 
+        return head
+
+
+        
