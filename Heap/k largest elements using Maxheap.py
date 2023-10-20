2f09a8cec10e05b83956e433618acf1cf6a9335a
TANISHKA GUPTA <84371065+thetanishkagupta@users.noreply.github.com>
2023-10-20 14:13:27 +0530

    K largest elements using Maxheap.py

---
+++ Heap/k largest elements using Maxheap.py
@@ -0,0 +1,19 @@
+
+import heapq
+def kLargest(arr, n, k):
+    maxheap = []
+    for i in range(n):
+        heapq.heappush(maxheap,-arr[i])
+    ans = []
+    for j in range(k):
+        ans.append(-heapq.heappop(maxheap))
+    return ans
+    
+A = [12, 5, 787, 1, 23]
+N = 5
+K = 2 
+result = kLargest(A, N, K)
+print(result)
+
+// Output: [787, 23]
+
